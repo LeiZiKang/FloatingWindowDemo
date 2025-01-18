888afc79ec2d7a6198478793a1fc22f051833c10
Levi Lei <levilei@esdlife.com>
2025-01-19 05:04:10 +0800

    Initial Commit

---
+++ FloatingWindowDemo/FloatingWindowDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FloatingWindowDemoApp.swift
+//  FloatingWindowDemo
+//
+//  Created by Lei Levi on 19/1/2025.
+//
+
+import SwiftUI
+
+@main
+struct FloatingWindowDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
